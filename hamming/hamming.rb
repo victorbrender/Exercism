d3006cd72d427000968507c41744e956eb81bd5a
Victor Brender <victor@2xwide.com>
2016-03-29 15:01:54 +0200

    Use enumerable count method to simplify difference calculation method

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -16,15 +16,9 @@ class Hamming
     end
 
     def calculate_hamming_difference(first_strand, second_strand)
-      hamming_difference = 0
-
-      (0..first_strand.size - 1).each do |i|
-        if first_strand[i] != second_strand[i]
-          hamming_difference += 1
-        end
+      (0..first_strand.size - 1).count do |i|
+        first_strand[i] != second_strand[i]
       end
-
-      hamming_difference
     end
   end
 end
