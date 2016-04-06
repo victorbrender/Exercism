c19175c19f7b1100eacebd3d47b79fd1b820954e
Victor Brender <victor@2xwide.com>
2016-04-06 15:06:47 +0200

    Implemented required methods

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,19 @@
 class Squares
+  VERSION = 2
+
   def initialize(n)
-    @n = n
+    @numbers = 0..n
   end
 
   def square_of_sum
+    @numbers.reduce(:+)**2
+  end
+
+  def sum_of_squares
+    @numbers.inject {|sum, i| sum + i**2}
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
