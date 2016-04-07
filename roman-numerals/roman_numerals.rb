62e5bfed57c1c49c7979703217f204e21e757ef4
Victor Brender <victor@2xwide.com>
2016-04-07 14:47:29 +0200

    [wip] Spike on converting a number to numerals

---
+++ roman-numerals/roman_numerals.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Fixnum
+  ROMAN_NUMBERS_DICTIONARY = {
+     1    => 'I',
+     5    => 'V',
+     10   => 'X',
+     50   => 'L',
+     100  => 'C',
+     500  => 'D',
+     1000 => 'M'
+  }
+
+  def to_roman
+ 
+  end
+end
